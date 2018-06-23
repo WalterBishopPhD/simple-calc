5fafd27028f34125f7e3cbb3992353e78e848f07
WalterBishopPhD <40410117+WalterBishopPhD@users.noreply.github.com>
2018-06-24 02:17:21 +0300

    Add files via upload

--- calc.js
+++ calc.js
@@ -3,19 +3,20 @@ function whichKey(ke){
   const display = document.getElementById("calc-io");
   const key = ke.key || ke.target.textContent;
 
-  if (/^[-\d\/\*\+\.\(\)]$/.test(key)) {
+  if ("0123456789/*-+.".includes(key)) {
     display.value += key;
   }
-  if (/^(=|Enter)$/.test(key)) {
+  if (["=", "Enter"].includes(key)) {
     display.value = eval(display.value);
   }
-  if (/^(Backspace|Delete)$/.test(key)) {
+  if (["Backspace", "Delete"].includes(key)) {
     display.value = display.value.slice(this.length, -1);
   }
-  if (/^Escape$/.test(key)) {
+  if (["Escape"].includes(key)) {
     display.value = "";
   }
 }
+
 document.addEventListener("keydown", whichKey);
 document.addEventListener("click",  whichKey);
 document.addEventListener("click", (e) => {document.activeElement.blur(e)});
