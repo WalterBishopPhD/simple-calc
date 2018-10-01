ccb48b9a4e61ac5c763a6a567f1c81242e5a12a4
WalterBishopPhD <40410117+WalterBishopPhD@users.noreply.github.com>
2018-10-01 22:02:35 +0300

    Add files via upload

--- calc.js
+++ calc.js
@@ -1,41 +1,24 @@
-// input
-function whichKey(ke){
-  const display = document.getElementById("calc-io");
+function whichKey(ke) {
+  const display = document.querySelector("#calc-io");
   const key = ke.key || ke.target.textContent;
 
-  if ("0123456789/*-+.()".includes(key)) {
+  if (/[0123456789/*\-+.()]/.test(key)) {
     display.value += key;
   }
-  if (["=", "Enter"].includes(key) && display.value !== "" && !display.value.match(/(\(\))/g)) {
-    proverkaNaDuraka();
+
+  if (/=|Enter/.test(key) && display.value !== "" && !display.value.match(/(\(\))/g)) {
     display.value = eval(display.value);
   }
-  if (["Backspace", "Delete", "CE"].includes(key)) {
-    display.value = display.value.slice(this.length, -1);
-    display.style.backgroundColor = "";
+
+  if (/Backspace|Delete|CE/.test(key)) {
+    display.value = display.value.slice(0, -1);
   }
-  if (["Escape", "AC"].includes(key)) {
+
+  if (/Escape|AC/.test(key)) {
     display.value = "";
-  }
-}
-function proverkaNaDuraka() {
-  const display = document.getElementById("calc-io");
-  if (display.value.match(/\(.+\)/g)) {
-    const dif = display.value.match(/[(]/g).length - display.value.match(/[)]/g).length;
-    
-    if (dif > 0) {
-      for (let i = 0; i < dif; ++i) {
-        display.value += ")";
-      }
-    }
-    if (dif < 0) {
-      display.style.backgroundColor = "red";
-    }
   }
 }
 
 document.addEventListener("keydown", whichKey);
 document.addEventListener("click",  whichKey);
-document.addEventListener("click", (e) => {document.activeElement.blur(e)});
-
-// document.addEventListener("click", (e) => {console.log(e)});
+document.addEventListener("click", e => document.activeElement.blur(e));
